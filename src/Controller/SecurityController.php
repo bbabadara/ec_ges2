0a94fbbe1a0dbdd9ea3d778f8d2bf2e0f3e9c9bb
bbabadara <bbabadara@gmail.com>
2025-03-11 23:20:13 +0000

    page login to tailwind

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,16 +2,26 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
     #[Route(path: '/security/login', name: 'app_security_login',methods:["GET","POST"])]
-    public function login(): Response
+    public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        return $this->render('security/login.html.twig');
+        // get the login error if there is one
+         $error = $authenticationUtils->getLastAuthenticationError();
+         // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig',[
+                    'controller_name' => 'LoginController',
+                    'last_username' => $lastUsername,
+                    'error'         => $error,
+                 ]);
     }
 
     #[Route(path: '/security/logout', name: 'app_security_logout',methods:["GET"])]
